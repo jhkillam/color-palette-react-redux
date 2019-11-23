2f2fe19c8c70ce11cd89f9dfa3f1b4dff801be1b
Joey Killam <jhkillam@gmail.com>
2019-11-23 13:05:19 -0600

    Updates

---
+++ precache-manifest.d10f6aa9c186f711bad94b8ec85552b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc93a5e02a524dcabb2e5bbf042f2240",
+    "url": "/color-palette-react-redux/index.html"
+  },
+  {
+    "revision": "d5d723ce9991e29d9add",
+    "url": "/color-palette-react-redux/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "262f1823282de26e4f22",
+    "url": "/color-palette-react-redux/static/js/2.a19e4495.chunk.js"
+  },
+  {
+    "revision": "d5d723ce9991e29d9add",
+    "url": "/color-palette-react-redux/static/js/main.a95ee41b.chunk.js"
+  },
+  {
+    "revision": "cb0dc9c92f279e52dfc7",
+    "url": "/color-palette-react-redux/static/js/runtime-main.f29bcd38.js"
+  }
+]);
